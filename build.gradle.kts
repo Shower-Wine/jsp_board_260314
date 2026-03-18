33e12a8ac6a738aa934978a36800adffc01d8a47
Shower-Wine <shower2126@gmail.com>
2026-03-19 00:26:28 +0900

    자카르타 API 추가, HomeServlet 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    // Source: https://mvnrepository.com/artifact/jakarta.servlet/jakarta.servlet-api
+    compileOnly("jakarta.servlet:jakarta.servlet-api:6.1.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
