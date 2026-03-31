4ab3d863536ffdce24b0b9735a69a2dc60d5a8f3
Shower-Wine <shower2126@gmail.com>
2026-03-31 16:12:38 +0900

    구구단 jsp 도입

--- src/main/java/org/example/boundedContext/gugudan/GugudanServlet.java
+++ src/main/java/org/example/boundedContext/gugudan/GugudanServlet.java
@@ -14,7 +14,7 @@ public class GugudanServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Rq rq = new Rq(req, resp);
-        
+
         int dan = rq.getIntParam("dan", 9);
         int limit = rq.getIntParam("limit", 9);
         rq.appendBody("<h1>== 구구단 %d단 ==</h1>\n".formatted(dan));
@@ -22,5 +22,13 @@ public class GugudanServlet extends HttpServlet {
         for(int i = 1; i <= limit; i++) {
             rq.appendBody("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
         }
+
+        rq.appendBody("""
+            <div class="box-1"></div>
+            
+            <style>
+                .box-1 { width:200px; height:200px; background-color: red; }
+            </style>
+            """);
     }
 }
