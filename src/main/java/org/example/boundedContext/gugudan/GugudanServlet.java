13c141af08b452916d3bb5f214bf07179aa23d41
Shower-Wine <shower2126@gmail.com>
2026-03-31 12:30:25 +0900

    구구단 서블릿에 rq 도입

--- src/main/java/org/example/boundedContext/gugudan/GugudanServlet.java
+++ src/main/java/org/example/boundedContext/gugudan/GugudanServlet.java
@@ -5,6 +5,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.example.boundedContext.global.base.Rq;
 
 import java.io.IOException;
 
@@ -12,16 +13,14 @@ import java.io.IOException;
 public class GugudanServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setCharacterEncoding("UTF-8"); // 들어오는 데이터를 UTF-8로 해석
-        resp.setCharacterEncoding("UTF-8"); // 완성된 HTML 결과물의 인코딩을 UTF-8로 하겠다.
-        resp.setContentType("text/html; charset=utf-8");
-
-        int dan = Integer.parseInt(req.getParameter("dan"));
-        int limit = Integer.parseInt(req.getParameter("limit"));
-        resp.getWriter().append("<h1>== 구구단 %d단 ==</h1>\n".formatted(dan));
+        Rq rq = new Rq(req, resp);
+        
+        int dan = rq.getIntParam("dan", 9);
+        int limit = rq.getIntParam("limit", 9);
+        rq.appendBody("<h1>== 구구단 %d단 ==</h1>\n".formatted(dan));
 
         for(int i = 1; i <= limit; i++) {
-            resp.getWriter().append("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
+            rq.appendBody("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
         }
     }
 }
